30477ca605b850e0a6d8b163f5a1bc6659a2e127
saikumar <gsaikumar082000@gmail.com>
2024-01-06 04:57:16 +0000

    login fill will created and developed logi functionality

---
+++ src/test/java/flipkart/LoginTest.java
@@ -0,0 +1,20 @@
+package flipkart;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class LoginTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
